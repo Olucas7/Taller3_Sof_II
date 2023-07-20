fca468f8572a5f8298b08f57a115083276c666f9
Oscar Lucas <olucas@espol.edu.ec>
2023-07-20 14:50:44 -0500

    Refactor the code

--- dining_manager.py
+++ dining_manager.py
@@ -1,24 +1,60 @@
-
-# Menú con ingredientes ecuatorianos y sus precios
+# Menú con comidas y sus precios (se le asigna el tema "Comida China" para ejemplo)
 menu = {
-    "1": {"name": "Humita", "price": 6},
-    "2": {"name": "Bolon de Verde", "price": 5},
-    "3": {"name": "Tigrillo", "price": 8},
+    "1": {"name": "Pollo con almendras", "price": 10, "category": "Comida China"},
+    "2": {"name": "Chow Mein", "price": 8, "category": "Comida China"},
+    "3": {"name": "Arroz Frito", "price": 7, "category": "Comida China"},
+    "4": {"name": "Wantan", "price": 6, "category": "Comida China"},
+    "5": {"name": "Bentobox", "price": 5, "category": "Comida China"},
 }
 
+# Categoría de platillo especial con precio más alto (se le asigna el tema "Especialidades del Chef" para ejemplo)
+special_category = "Especialidades del Chef"
+special_category_surcharge = 0.05  # 5% de recargo para platillos de la categoría especial
+
 def display_menu():
-    print("Bienvenido(a) al Administrador de Comidas")
+    print("Bienvenido(a) al Administrador de Experiencias Gastronómicas")
     print("Menú:")
     for key, item in menu.items():
-        print(f"{key}. {item['name']} - ${item['price']:.2f}")
+        print(f"{key}. {item['name']} - ${item['price']:.2f} ({item['category']})")
     print()
+
+def validate_quantity(quantity):
+    try:
+        quantity = int(quantity)
+        if quantity <= 0 or quantity > 100:
+            raise ValueError
+        return quantity
+    except ValueError:
+        return -1
 
 def calculate_total(order_items):
     total_cost = 0
+    special_category_items = 0
+
     for item, quantity in order_items.items():
-        if item in menu and quantity > 0:
-            total_cost += menu[item]['price'] * quantity
-    return total_cost
+        if item in menu:
+            meal_price = menu[item]['price']
+            if menu[item]['category'] == special_category:
+                special_category_items += quantity
+                meal_price += meal_price * special_category_surcharge
+            total_cost += meal_price * quantity
+
+    if len(order_items) == 0 or total_cost <= 0:
+        return -1
+
+    if len(order_items) >= 5:
+        total_cost *= 0.9
+
+    if len(order_items) >= 10:
+        total_cost *= 0.8
+
+    if total_cost > 50:
+        total_cost -= 10
+
+    if total_cost > 100:
+        total_cost -= 25
+
+    return int(total_cost)
 
 def main():
     display_menu()
@@ -29,14 +65,25 @@ def main():
         if not item:
             break
 
-        quantity = int(input("Ingrese la cantidad: "))
+        quantity = input("Ingrese la cantidad: ")
+        quantity = validate_quantity(quantity)
+        if quantity == -1:
+            print("Cantidad inválida. Por favor, ingrese una cantidad válida (entero positivo entre 1 y 100).")
+            continue
+
         if item in menu:
             order_items[item] = order_items.get(item, 0) + quantity
         else:
             print("Número de platillo inválido. Por favor, inténtelo nuevamente.")
 
     total_cost = calculate_total(order_items)
-    print(f"Total de su orden: ${total_cost:.2f}")
+    if total_cost == -1:
+        print("Orden cancelada debido a datos inválidos.")
+    else:
+        print("Resumen de la orden:")
+        for item, quantity in order_items.items():
+            print(f"{menu[item]['name']} - Cantidad: {quantity}")
+        print(f"Total de la orden: ${total_cost}")
 
 if __name__ == "__main__":
     main()
--- test_dining_manager.py
+++ test_dining_manager.py
@@ -1,39 +1,31 @@
 # test_dining_manager.py
 
-from dining_manager import calculate_total
+import pytest
+from dining_manager import validate_quantity, calculate_total
 
-# Menú con ingredientes ecuatorianos y sus precios
-menu = {
-    "1": {"name": "Humita", "price": 6},
-    "2": {"name": "Bolon de Verde", "price": 5},
-    "3": {"name": "Tigrillo", "price": 8},
-}
+def test_validate_quantity():
+    assert validate_quantity(0) == -1
+    assert validate_quantity(-5) == -1
+    assert validate_quantity(101) == -1
+    assert validate_quantity(3) == 3
+    assert validate_quantity(100) == 100
 
-# Casos de prueba para la función calculate_total
+def test_calculate_total():
+    # Test with empty order_items
+    assert calculate_total({}) == -1
 
-def test_calculate_total_sin_items():
-    # Prueba con order_items vacío
-    assert calculate_total({}) == 0
+    # Test with one item
+    order_items_1 = {"1": 2}  # 2 Pollo con almendras
+    assert calculate_total(order_items_1) == 10 * 2
 
-def test_calculate_total_un_item():
-    # Prueba con un solo platillo
-    order_items_1 = {"1": 2}  # 2 Humitas
-    assert calculate_total(order_items_1) == 6 * 2  # El precio de una humita es 6, por lo tanto, el total debe ser 6 * 2 = 12
+    # Test with multiple items
+    order_items_2 = {"1": 2, "2": 1, "3": 3}  # 2 Pollo con almendras, 1 Chow Mein, 3 Arroz Frito
+    assert calculate_total(order_items_2) == 10 * 2 + 8 * 1 + 7 * 3
 
-def test_calculate_total_varios_items():
-    # Prueba con varios platillos
-    order_items_2 = {"1": 2, "2": 1, "3": 3}  # 2 Humitas, 1 Bolon de Verde, 3 Tigrillos
-    assert calculate_total(order_items_2) == 6*2 + 5*1 + 8*3  # El total debe ser la suma de los precios de los platillos seleccionados
+    # Test with special category
+    order_items_3 = {"1": 1, "3": 1}  # 1 Pollo con almendras, 1 Arroz Frito (ambos de categoría especial)
+    assert calculate_total(order_items_3) == int((10 + 10 * 0.05) + (7 + 7 * 0.05))
 
-def test_calculate_total_item_invalido():
-    # Prueba con un número de platillo inválido (no está en el menú)
-    order_items_3 = {"4": 1}  # Número de platillo inválido
-    assert calculate_total(order_items_3) == 0  # Dado que el platillo no está en el menú, el costo total debe ser 0
-
-def test_calculate_total_cantidad_negativa():
-    # Prueba con cantidad negativa para un platillo
-    order_items_4 = {"1": -2}  # Cantidad negativa para Humita
-    assert calculate_total(order_items_4) == 0  # Dado que la cantidad es negativa, el costo total debe ser 0
-
-if __name__ == "__main__":
-    pytest.main()
+    # Test with discount for more than 5 items
+    order_items_4 = {"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}  # 5 platillos diferentes
+    assert calculate_total(order_items_4) == int(36 * 0.9)
